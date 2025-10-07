e5bdce4d79cf9c47cc66fd4e325ddbd76dd3d4f5
K M Imtiaz Hossain <142367575+sakhorimtiaz@users.noreply.github.com>
2025-10-07 15:52:28 +0600

    Update pandas.py

--- week_2/pandas.py
+++ week_2/pandas.py
@@ -1,1 +1,27 @@
+import pandas as pd
+def pandas_series():
+    students = ['Alice', 'Jack', 'Molly']
+    student = ['Alice', 'Jack', None]
+    numbers = [1, 2, 3]
+    number = [1, 2, None]
+    print(pd.Series(students))
+    print(pd.Series(numbers))
+    print(pd.Series(student))
+    print(pd.Series(number))
 
+import numpy as np
+def checking_nan_and_none():
+    print(np.nan == np.nan)
+    print(np.nan == None)
+    print(np.isnan(np.nan))
+def dictionary_and_tuples():
+    students_scores = {'Alice': 'Physics',
+                       'Jack': 'Chemistry',
+                       'Molly': 'English'}
+    print(pd.Series(students_scores))
+    students = [("Alice", "Brown"), ("Jack", "White"), ("Molly", "Green")]
+    print(pd.Series(students))
+    s = pd.Series(["Physics", "Chemistry", "English"], index=["Alice", "Jack", "Molly"])
+    print(s)
+    p = pd.Series(students_scores, index=["Alice", "Molly", "Sam"])
+    print(p)
