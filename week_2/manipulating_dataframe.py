bdb3b41e6dd4a1762c43b57ae38a346c6973df14
K M Imtiaz Hossain <142367575+sakhorimtiaz@users.noreply.github.com>
2025-10-09 14:43:24 +0600

    Update manipulating_dataframe.py

--- week_2/manipulating_dataframe.py
+++ week_2/manipulating_dataframe.py
@@ -1,1 +1,9 @@
-
+# process1_not_a_good_option
+df=pd.read_csv(r"C:\Users\THINKPAD\Downloads\presidents.csv",encoding="utf-8")
+print(df.head())
+df["First"]=df["President"]
+print(df.head())
+df["First"]=df["First"].replace("[ ].*","",regex=True)
+print(df.head())
+del(df["First"])
+print(df.head())
