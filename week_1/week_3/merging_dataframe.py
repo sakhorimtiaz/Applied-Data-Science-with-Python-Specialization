93640c237b2d018fc67f4c87b4aa388a4764d8be
K M Imtiaz Hossain <142367575+sakhorimtiaz@users.noreply.github.com>
2025-10-10 11:22:29 +0600

    Update merging_dataframe.py

--- week_1/week_3/merging_dataframe.py
+++ week_1/week_3/merging_dataframe.py
@@ -1,5 +1,5 @@
 import pandas as pd
-def union_intersect_1():
+def union_intersect_example_1():
     staff_df = pd.DataFrame([{'Name': 'Kelly', 'Role': 'Director of HR'},
                              {'Name': 'Sally', 'Role': 'Course liasion'},
                              {'Name': 'James', 'Role': 'Grader'}])
@@ -22,3 +22,33 @@ def union_intersect_1():
     staff_df = staff_df.reset_index()
     student_df = student_df.reset_index()
     pd.merge(staff_df, student_df, how="right", on="Name")
+
+def union_intersect_example_2():
+    staff_df = pd.DataFrame([{'Name': 'Kelly', 'Role': 'Director of HR',
+                              'Location': 'State Street'},
+                             {'Name': 'Sally', 'Role': 'Course liasion',
+                              'Location': 'Washington Avenue'},
+                             {'Name': 'James', 'Role': 'Grader',
+                              'Location': 'Washington Avenue'}])
+    student_df = pd.DataFrame([{'Name': 'James', 'School': 'Business',
+                                'Location': '1024 Billiard Avenue'},
+                               {'Name': 'Mike', 'School': 'Law',
+                                'Location': 'Fraternity House #22'},
+                               {'Name': 'Sally', 'School': 'Engineering',
+                                'Location': '512 Wilson Crescent'}])
+    pd.merge(staff_df, student_df, how="left", on="Name")
+
+def union_intersect_example_3():
+    staff_df = pd.DataFrame([{'First Name': 'Kelly', 'Last Name': 'Desjardins',
+                              'Role': 'Director of HR'},
+                             {'First Name': 'Sally', 'Last Name': 'Brooks',
+                              'Role': 'Course liasion'},
+                             {'First Name': 'James', 'Last Name': 'Wilde',
+                              'Role': 'Grader'}])
+    student_df = pd.DataFrame([{'First Name': 'James', 'Last Name': 'Hammond',
+                                'School': 'Business'},
+                               {'First Name': 'Mike', 'Last Name': 'Smith',
+                                'School': 'Law'},
+                               {'First Name': 'Sally', 'Last Name': 'Brooks',
+                                'School': 'Engineering'}])
+    pd.merge(staff_df, student_df, how="inner", on=["First Name", "Last Name"])
