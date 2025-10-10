e3f875afaeeab11f551abd622f9001ca96b6ef42
K M Imtiaz Hossain <142367575+sakhorimtiaz@users.noreply.github.com>
2025-10-10 14:47:40 +0600

    Update panda_idioms.py

--- week_3/panda_idioms.py
+++ week_3/panda_idioms.py
@@ -10,4 +10,17 @@ def pandorable():
                 .dropna()
                 .set_index(["STNAME", "CTYNAME"])
                 .rename(columns={"ESTIMATESBASE2010": "Estimates Base 2010"}))
-    print(readable)
+    return readable
+
+def conventional():
+    df = pd.read_csv(r"C:\Users\THINKPAD\Downloads\census.csv")
+
+    new_df=df[df["SUMLEV"]==50]
+    new_df.set_index(["STNAME", "CTYNAME"],inplace=True)
+    new_df.rename(columns={"ESTIMATESBASE2010": "Estimates Base 2010"})
+    return new_df
+
+t1=timeit.timeit(pandorable,number=10)
+t2=timeit.timeit(conventional,number=10)
+print(t1)
+print(t2)
