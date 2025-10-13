48621e378ab61fc04c32adc00a16339807707ac3
K M Imtiaz Hossain <142367575+sakhorimtiaz@users.noreply.github.com>
2025-10-13 16:19:51 +0600

    Update scales.py

--- week_3/scales.py
+++ week_3/scales.py
@@ -13,7 +13,7 @@ def ordered_category():
     df = pd.DataFrame(['A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D'],
                       index=['excellent', 'excellent', 'excellent', 'good', 'good', 'good',
                              'ok', 'ok', 'ok', 'poor', 'poor'], columns=["Grades"])
-    my_categories = pd.CategoricalDtype(categories=["A+", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D+", "D"],
+    my_categories = pd.CategoricalDtype(categories=['D', 'D+', 'C-', 'C', 'C+', 'B-', 'B', 'B+', 'A-', 'A', 'A+'],
                                         ordered=True)
     grades = df["Grades"].astype(my_categories)
     df = df[df["Grades"] > "C"]
