283deadcfb74fe9b1000bc6e0a2e84dd539d5d34
K M Imtiaz Hossain <142367575+sakhorimtiaz@users.noreply.github.com>
2025-10-14 09:40:44 +0600

    Create date_time.py

---
+++ week_3/date_time.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+#print(pd.Timestamp("14/10/2025 9:2m"))
+#print(pd.Timestamp(year=2025,month=10,day=14,hour=9,minute=10,second=35))
+#print(pd.Timestamp(year=2025,month=10,day=14,hour=9,minute=10,second=35).isoweekday())
+#print(pd.Timestamp(year=2025,month=10,day=14,hour=9,minute=10,second=35).month)
+
+#print(pd.Period("10/2005"))
+#print(pd.Period("10/2005")+2)
+#print(pd.Period("10/14/2005")+2)
+#print(pd.Period("14/10/2025 9:2am")-2)
