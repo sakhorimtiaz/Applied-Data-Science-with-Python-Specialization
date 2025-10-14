1510fbda85e0f7193a04808d88367363e4b39ac6
K M Imtiaz Hossain <142367575+sakhorimtiaz@users.noreply.github.com>
2025-10-14 09:58:34 +0600

    Update date_time.py

--- week_3/date_time.py
+++ week_3/date_time.py
@@ -10,3 +10,12 @@ import numpy as np
 #print(pd.Period("10/2005")+2)
 #print(pd.Period("10/14/2005")+2)
 #print(pd.Period("14/10/2025 9:2am")-2)
+def date_time_index_period_index():
+    t1 = pd.Series(data=list("abc"),
+                   index=[pd.Timestamp('2016-09-01'), pd.Timestamp('2016-09-02'), pd.Timestamp('2016-09-03')])
+    print(t1)
+    print(t1.index)
+    t2 = pd.Series(list('def'), [pd.Period('2016-09'), pd.Period('2016-10'),
+                                 pd.Period('2016-11')])
+    print(t2)
+    print(t2.index)
